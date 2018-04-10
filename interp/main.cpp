c1579ac908e75d71a7cbf698d456183d1a7e5380
dvynjli <dvynjli@gmail.com>
2018-04-10 09:43:56 +0530

    clean up

--- interp/main.cpp
+++ interp/main.cpp
@@ -8,31 +8,37 @@ class IntVar {
         IntVar(string name, int val) : name(name), val(val) {}
 };
 
-struct VerifierPass : public ModulePass {
-    static char ID;
-    VerifierPass() : ModulePass(ID) {}
-
+class VerifierPass : public ModulePass {
     bool runOnModule (Module &M) {
         errs() << "LLVM pass is running\n";
         
         ap_manager_t *ap_man = box_manager_alloc();                     //TODO: depend upon command line arg
-        
+        ap_environment_t *env = initEnvironment(M);
     }
 
     ap_environment_t * initEnvironment(Module &M) {
-        vector<IntVar> intVars;
+        vector<string> intVars;
         for (auto it = M.global_begin(); it != M.global_end(); it++){
             print_msg(2, "Global var: ", it->getName());
             if (it->getType()->getElementType()->isIntegerTy()) {
-                intVars.push_back(IntVar(it->getName(), 0));
+                intVars.push_back(it->getName());
             }
         }
         print_debug_msg(2, "Total global var = ", to_string(intVars.size()).c_str());
-        ap_var_t ints[intVars.size()];
+        ap_var_t intAp[intVars.size()];
+        int i = 0;
+        for (auto it = intVars.begin(); it != intVars.end(); it++, i++){
+            intAp[i] = (ap_var_t)(it->c_str());
+        }
+        ap_var_t floatAp[0];
         
-        return nullptr;
-        //return ap_environment_alloc(intArr, intArr.size(), f)
+        //return nullptr;
+        return ap_environment_alloc(intAp, intVars.size(), floatAp, 0);
     }
+
+    public:
+        static char ID;
+        VerifierPass() : ModulePass(ID) {}
 };
 
 char VerifierPass::ID = 0;
