72783207bc2dbef6a99aabc967dba6c084742293
dvynjli <dvynjli@gmail.com>
2019-02-19 13:08:54 +0530

    Using linexpr to change domain

--- README.md
+++ README.md
@@ -8,7 +8,9 @@ Python based bstract Interpreter for pthread programs. Creates a LLVM pass to ve
     cmake ..
     make
 ```
+Might need to run ```make``` as sudo
 
-#Running the pass
+# Running the pass
+```
 opt -load build/interp/VerifierPass.so -verifier thread_test.ll
-
+```
--- interp/main.cpp
+++ interp/main.cpp
@@ -9,7 +9,7 @@ class IntVar {
 };
 
 class VerifierPass : public ModulePass {
-    /* TODO: Should I create threads as a set??
+    /* TODO: Should I create threads as a set or vector??
         Vector- interferences of t1 with t1 will be explored in case more than one thread of same func are present
         Set and interference of each thread with itself are also explored, support to inf threads of same func can be added 
     */
@@ -21,39 +21,79 @@ class VerifierPass : public ModulePass {
         ap_manager_t *man = initApManager();
         fprintf(stderr, "Init Env\n");
         ap_environment_t *env = initEnvironment(M);
-        ap_environment_fdump(stdout, env);
-         //ap_var_t var = ap_environment_var_of_dim(env, 0);
+        ap_environment_fdump(stderr, env);
+        // ap_var_t var = ap_environment_var_of_dim(env, 0);
+        // fprintf(stderr, "var: %s\n", var);
         fprintf(stderr, "creating top\n");
-        ap_abstract1_t top = ap_abstract1_top(man, env);
-        ap_abstract1_fdump(stderr, man,  &top);
-
-        initThreadDetails(M);
+        ap_abstract1_t abs_val = ap_abstract1_top(man, env);
+        // ap_abstract1_fdump(stderr, man,  &top);
         
+        // initThreadDetails(M);
+        performTrasfer(man, env, abs_val);
+        ap_abstract1_fdump(stderr, man,  &abs_val);
+    }
+
+    void performTrasfer(ap_manager_t *man, ap_environment_t *env, ap_abstract1_t abs_val) {
+        /* assign x = 1 */
+        fprintf(stderr, "Assigning x = 1\n");
+        ap_linexpr1_t expr = ap_linexpr1_make(env, AP_LINEXPR_SPARSE, 0);
+        ap_linexpr1_set_list(&expr, AP_CST_S_INT, 1, AP_END);
+        ap_linexpr1_fprint(stderr, &expr);
+        ap_var_t var = ap_environment_var_of_dim(env, 0);
+        abs_val = ap_abstract1_assign_linexpr(man, true, &abs_val, var, &expr, NULL);
+        fprintf(stderr, "assigned linexpr to x\n");
+        ap_abstract1_fprint(stderr, man, &abs_val);
+        // ap_linexpr1_clear(&expr);
+
+        /* assign y = x + 10 */
+        fprintf(stderr, "Assigning y = x + 10\n");
+        ap_linexpr1_set_list(&expr, AP_COEFF_S_INT, 1, "x", AP_CST_S_INT, 10, AP_END);
+        ap_linexpr1_fprint(stderr, &expr);
+        var = ap_environment_var_of_dim(env, 1);
+        abs_val = ap_abstract1_assign_linexpr(man, true, &abs_val, var, &expr, NULL);
+        fprintf(stderr, "assigned linexpr to x\n");
+        ap_abstract1_fprint(stderr, man, &abs_val);
+        // ap_linexpr1_clear(&expr);
+
+        /* assign x = x + y */
+        fprintf(stderr, "Assigning x = x + y\n");
+        ap_linexpr1_set_list(&expr, AP_COEFF_S_INT, 1, "x", AP_COEFF_S_INT, 1, "y", AP_CST_S_INT, 0, AP_END);
+        ap_linexpr1_fprint(stderr, &expr);
+        var = ap_environment_var_of_dim(env, 0);
+        abs_val = ap_abstract1_assign_linexpr(man, true, &abs_val, var, &expr, NULL);
+        fprintf(stderr, "assigned linexpr to x\n");
+        ap_abstract1_fprint(stderr, man, &abs_val);
+        
+        ap_linexpr1_clear(&expr);
+
     }
 
     ap_environment_t * initEnvironment(Module &M) {
         vector<string> intVars;
         for (auto it = M.global_begin(); it != M.global_end(); it++){
-            fprintf(stderr, "Global var: %s\n", it->getName());
-            fprintf(stderr, "%d\n", it->getValueType()->getTypeID());
+            // cerr << "Global var: " << it->getName() << endl;
+            fprintf(stderr, "Global var: %s of type: %d\n", it->getName(), it->getValueType()->getTypeID());
             if (it->getValueType()->isIntegerTy()) {
                 intVars.push_back(it->getName());
             }
-            if (it->getValueType()->isStructTy()){
-                fprintf(stderr, "Struct: %s of type", it->getName());
-                // if (auto st = dyn_cast<StructType>(it->getValueType())){
-                //     fprintf(stderr, "cast\n");
-                // }
-                fprintf(stderr, "%s\n", it->getValueType()->getStructName());
-            }
+
+            // Needed for locks
+            // if (it->getValueType()->isStructTy()){
+            //     fprintf(stderr, "Struct: %s of type", it->getName());
+            //     // if (auto st = dyn_cast<StructType>(it->getValueType())){
+            //     //     fprintf(stderr, "cast\n");
+            //     // }
+            //     fprintf(stderr, "%s\n", it->getValueType()->getStructName());
+            // }
 
         }
-        fprintf(stderr, "DEBUG: Total global var = %d\n", intVars.size());
+        fprintf(stderr, "DEBUG: Total global var = %lu\n", intVars.size());
         ap_var_t intAp[intVars.size()];
         int i = 0;
         for (auto it = intVars.begin(); it != intVars.end(); it++, i++){
             intAp[i] = (ap_var_t)(it->c_str());
         }
+        //TODO: use if providing support for floats
         ap_var_t floatAp[0];
         
         //return nullptr;
@@ -62,13 +102,20 @@ class VerifierPass : public ModulePass {
 
     ap_manager_t* initApManager()
     {
-        //TODO: depend upon command line arg
+        //TODO: parameterize by command line arg
         return box_manager_alloc();
+    }
+
+    //  call approprproate function for the inst passed
+    void checkInstruction(Instruction* inst){
+
     }
 
     void initThreadDetails(Module &M) {
         //find main function
         Function *mainF;
+
+        //TODO: llvm should have a better way of finding main function
         for(auto funcItr = M.begin(); funcItr != M.end(); funcItr++) {
             if (funcItr->getName() == "main"){
                 mainF = (Function*)funcItr;
@@ -95,8 +142,25 @@ class VerifierPass : public ModulePass {
                                 threads.push_back(newThread);
                                 funcQ.push(newThread);
                             }
+
+                        }
+                        //TODO: what should be done for non-pthread create function call
+                    }
+                    else
+                        checkInstruction(&(*it));
+                    /*  if (BinaryOperator *binOp = dyn_cast<BinaryOperator>(it)) {
+                        if (binOp->getOpcode() == Instruction::Add) {
+                            // fprintf(stderr, "Found add inst: ");
+                            // binOp->dump();
+
+                        }
+                        else{
+                            // fprintf(stderr, "Op found: %s\n", binOp->getOpcodeName());
                         }
                     }
+                    else {
+                        // fprintf(stderr, "Opcode of instruction: %d %s\n", it->getOpcode(), it->getOpcodeName());
+                    } */
                 }
             }
         }
