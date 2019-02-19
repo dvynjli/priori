7f6f3eaa691b498389afd8ff3189ce3eef5eb067
dvynjli <dvynjli@gmail.com>
2019-02-19 13:09:37 +0530

    basic test

---
+++ tests/test1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <assert.h>
+
+int x, y;
+
+void* fun2(void * arg){
+	x = y + 1;
+	return NULL;
+}
+
+void* fun1(void * arg){
+	pthread_t t1;
+	pthread_create(&t1, NULL, fun2, NULL);
+	y = 10;
+	pthread_join(t1, NULL);
+	return NULL;
+}
+
+int main () {
+	pthread_t t1,t2;
+	pthread_create(&t1, NULL, fun1, NULL);
+	pthread_create(&t2, NULL, fun1, NULL);
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+	
+	return 0;
+}
