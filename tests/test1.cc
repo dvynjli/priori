def6e73f85a41ecf34f65e871c282d9c3cc8c6ee
dvynjli <dvynjli@gmail.com>
2019-03-14 20:04:25 +0530

    llvm -O1 passes

--- tests/test1.cc
+++ tests/test1.cc
@@ -14,6 +14,7 @@ void* fun2(void * arg){
 	// atomic_fetch_add_explicit(&x, 1, memory_order_acq_rel);
 	x.store(y.load(memory_order_relaxed)+b, memory_order_relaxed);
 	int a = 10;
+	b = 5;
 	y.store(a+x.load(memory_order_relaxed), memory_order_relaxed);
 	// y.load(memory_order_relaxed);
 	return NULL;
@@ -23,6 +24,8 @@ void* fun1(void * arg){
 	pthread_t t1;
 	pthread_create(&t1, NULL, fun2, NULL);
 	x.store(b, memory_order_relaxed);
+	// y = b + 1;
+	// x = y + 1;
 	pthread_join(t1, NULL);
 	return NULL;
 }
