6692b98d74336863ab00b703768f60ed9455a4e5
dvynjli <dvynjli@gmail.com>
2019-11-11 12:11:37 +0530

    added testcases for RA

--- tests/test35.cc
+++ tests/test35.cc
@@ -14,17 +14,10 @@ void* fun1(void * arg){
 }
 
 void* fun2(void * arg){
-	x.store(2, memory_order_release);
-	y.store(2, memory_order_release);
-	return NULL;
-}
-
-void* fun3(void * arg){
-	int tmp1 = x.load(memory_order_acquire);
-	int tmp2 = y.load(memory_order_acquire);
-	int tmp3 = tmp1 && tmp2;
-	if (tmp3)
-		assert(tmp1 ==0);
+	int tmp1 = y.load(memory_order_acquire);
+	int tmp2 = x.load(memory_order_acquire);
+	// tmp2 == 1 => tmp1 == 1 should fail
+	assert((tmp2!=1) || (tmp1==1));
 	return NULL;
 }
 
@@ -33,9 +26,7 @@ int main () {
 	pthread_t t1,t2,t3;
 	pthread_create(&t1, NULL, fun1, NULL);
 	pthread_create(&t2, NULL, fun2, NULL);
-	pthread_create(&t3, NULL, fun3, NULL);
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
-	pthread_join(t3, NULL);
 	return 0;
 }
