e2e21011b44458d5b2b452c2238b406a9e3e29d3
dvynjli <dvynjli@gmail.com>
2020-03-18 15:21:03 +0530

    updated benchmarks

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -4,6 +4,7 @@ from subprocess import Popen, PIPE
 import subprocess
 import os
 import sys
+import datetime
 
 domain = 'interval' 	# options are interval, octagon
 num_runs = 4
@@ -12,23 +13,24 @@ num_runs = 4
 vbmc_unfenced = ['bakery_assume_unfenced', 'burns_assume_unfenced', 'dekker_assume_unfenced', 
 						'lamport_assume_unfenced', 'peterson_assume_unfenced']
 vbmc_fenced = ['bakery_assume_fenced', 'burns_assume_fenced', 'peterson_assume_fenced', 
-				'tbar_fenced', 'tbar_fenced_2']
+				'tbar_fenced', 'tbar_fenced_2', 'lamport_assume_fenced']
 # following benchmarks do not have errors and are from Tracer
 tracer_no_bug = ['CO-2+2W_2', 'CO-2+2W_3', 'CO-2+2W_5', 'fibonacci', 'dijkstra']
 benchmarks = []
 
-if len(sys.argv) == 1 or sys.argv[1] == '-vbmc-unfenced' or sys.argv[1] == '-vuf':
-	benchmarks = vbmc_unfenced
+if len(sys.argv) == 1 or sys.argv[1] == '-all' or sys.argv[1] == '-a':
+	benchmarks = vbmc_unfenced + vbmc_fenced + tracer_no_bug
 elif sys.argv[1] == '-vbmc-febced' or sys.argv[1] == '-vf':
 	benchmarks = vbmc_fenced
 elif sys.argv[1] == '-tracer-no-bug' or sys.argv[1] == '-tnb':
 	benchmarks = tracer_no_bug
-elif sys.argv[1] == '-all' or sys.argv[1] == '-a':
-	benchmarks = vbmc_unfenced + vbmc_fenced + tracer_no_bug
+elif sys.argv[1] == '-vbmc-unfenced' or sys.argv[1] == '-vuf':
+	benchmarks = vbmc_unfenced
 else:
 	print('Flags: ""/-vuf/-vf/-tnb/-a')
 	exit(0)
 
+print('Results generated at ', str(datetime.datetime.now()))
 print('Name, Time, Iterations')
 
 for benchmark in benchmarks:
@@ -54,7 +56,7 @@ for benchmark in benchmarks:
 		
 		start_index_of_iter = end_index_of_time + str(err)[end_index_of_time: ].find('#iterations: ') + len('#iterations: ')
 		end_index_of_iter = start_index_of_iter + str(err)[start_index_of_iter: ].find(r'\n')
-		print(benchmark, ',', str(err)[start_index_of_time : end_index_of_time], ',', str(err)[start_index_of_iter : end_index_of_iter])
+		print(benchmark, ',', time/runs, ',', str(err)[start_index_of_iter : end_index_of_iter])
 		
 duration = .1  # seconds
 freq = 440  # Hz
