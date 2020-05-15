bc657541b33a15cc42b052800ccc28ab0709ce69
dvynjli <dvynjli@gmail.com>
2020-05-15 21:12:06 +0530

    directory for final benchmarks

---
+++ tests/benchmarks_set/CO-2+2W_2.cc
@@ -0,0 +1,42 @@
+/* Copyright (C) 2018 Tuan Phong Ngo
+ * This file is part of TRACER */
+
+/* There are N weak traces */
+
+#include "verify.h"
+
+using namespace std;
+
+#define N 2
+
+atomic_int vars;
+
+void *t1(void *arg){
+  int tid = 1;
+  vars.store(tid, memory_order_release);
+  vars.store(tid, memory_order_release);
+  return NULL;
+}
+
+void *t2(void *arg){
+  int tid = 2;
+  vars.store(tid, memory_order_release);
+  vars.store(tid, memory_order_release);
+  return NULL;
+}
+
+
+int main(){
+  pthread_t ts1, ts2;
+  
+  pthread_create(&ts1, NULL, t1, NULL);
+  pthread_create(&ts2, NULL, t2, NULL);
+  
+  pthread_join(ts1, NULL);
+  pthread_join(ts2, NULL);
+  
+  int v1 = vars.load(memory_order_acquire);
+  int v2 = (v1 == N+1);
+  if (v2 == 1) assert(0);
+  return 0;
+}
